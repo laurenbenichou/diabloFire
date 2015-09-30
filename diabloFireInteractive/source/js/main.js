c70ca676568ea40668c5b812ced6f565425ddded
Lauren Benichou <laurenbenichou@gmail.com>
2015-09-30 10:32:11 -0700

    added all images

--- diabloFireInteractive/source/js/main.js
+++ diabloFireInteractive/source/js/main.js
@@ -1,1 +1,12 @@
-//require slick-carousel/slick
+//= require imageMap-resizer/js/imageMapResizer
+$(document).ready(function() {
+    $('map').imageMapResize();
+    // $('#spread').maphilight()
+    // $('area').on('mouseover', function(){
+    // 	console.log(this)
+    // 	$(this).maphilight({
+    // 		fill: true,
+    // 		fillColor: '008800'
+    // 		});
+    // })
+});
